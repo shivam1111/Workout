da5c9995cc410718cae2eb0e251b657d9099e4da
Shivam Goyal <shivam1111@gmail.com>
2021-12-09 09:00:07 +0530

    Page 350

--- app/src/main/java/com/hfad/workout/MainActivity.java
+++ app/src/main/java/com/hfad/workout/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hfad.workout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -11,14 +12,17 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WorkoutDetailFragment frag = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);
-        frag.setWorkout(1);
     }
 
     @Override
     public void itemClicked(long id){
         // Code
-        Log.d("Fragement","Fragment click checked");
+        WorkoutDetailFragment details = new WorkoutDetailFragment();
+        FragmentTransaction ft  = getSupportFragmentManager().beginTransaction();
+        details.setWorkout(id);
+        ft.replace(R.id.fragment_container,details);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
     }
-
 }
--- app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -2,6 +2,7 @@ package com.hfad.workout;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -63,6 +64,10 @@ public class WorkoutDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (savedInstanceState != null){
+            this.workoutId = savedInstanceState.getLong("workoutId");
+        }
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
@@ -83,5 +88,11 @@ public class WorkoutDetailFragment extends Fragment {
             TextView description = (TextView) view.findViewById(R.id.textDescription);
             description.setText(workout.getDescription());
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong("workoutId",workoutId);
     }
 }
